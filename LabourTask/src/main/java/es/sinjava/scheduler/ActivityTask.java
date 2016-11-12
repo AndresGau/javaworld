f6f46ba36964defab49fac494cd20d84821d40e7
Andrés <andgau@gmail.com>
2016-11-12 16:25:47 +0100

    otro cambio extraño

--- LabourTask/src/main/java/es/sinjava/scheduler/ActivityTask.java
+++ LabourTask/src/main/java/es/sinjava/scheduler/ActivityTask.java
@@ -1,13 +1,7 @@
 package es.sinjava.scheduler;
 
-import java.io.IOException;
 import java.util.TimerTask;
-import java.util.logging.FileHandler;
-import java.util.logging.Formatter;
-import java.util.logging.Handler;
-import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 public class ActivityTask extends TimerTask {
 
